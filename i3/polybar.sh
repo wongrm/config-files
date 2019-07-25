3e5f46be262d4244d51fa934f42343f30fba6bc8
Mathieu Wong-Rose <Mathieu.Wong-Rose@pwgsc-tpsgc.gc.ca>
2019-07-25 14:48:04 -0700

    poly WIP

---
+++ i3/polybar.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+#Terminate running bar
+killall -q polybar
+
+#Wait till process shutdown
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+#Launch polybar
+polybar example &
